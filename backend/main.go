c137fd076d9d357d134acd6c94ca0a78ae667cc0
robert.smol <robert.smol@stereoit.com>
2024-12-15 21:45:16 +0000

    Added simple backend

---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	fmt.Println("Server listening on port 8080")
+	http.ListenAndServe(":8080", r)
+}
