b40be826bb174f9b9d7a9e326fb2225c93702c0b
robert.smol <robert.smol@stereoit.com>
2025-01-02 10:57:52 +0000

    Added simple Figma export to tailwind configuration

--- frontend/src/routes/test-petr/+page.svelte
+++ frontend/src/routes/test-petr/+page.svelte
@@ -1,5 +1,114 @@
 <h1>Test page of Petr</h1>
 
+
 <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+
+<div>
+<div class="h-[316px] flex-col justify-start items-start gap-3 inline-flex">
+  <div class="w-[360px] px-4 justify-start items-start gap-2 inline-flex">
+    <div class="grow shrink basis-0 flex-col justify-start items-start gap-1 inline-flex">
+      <div class="self-stretch justify-start items-start inline-flex">
+        <div class="grow shrink basis-0 text-[#141c26] text-base font-medium font-['Roboto'] leading-normal tracking-tight">Elektronické systémy</div>
+        <div class="justify-start items-start gap-1 flex"></div>
+      </div>
+    </div>
+  </div>
+  <div class="flex-col justify-start items-start flex">
+    <div class="w-[360px] h-[280px] shadow-[0px_4px_12px_0px_rgba(0,0,0,0.08)] flex-col justify-end items-center gap-2.5 flex">
+      <div class="w-[360px] h-[280px] relative bg-white  overflow-hidden"></div>
+    </div>
+    <div class="w-[360px] h-14 bg-white flex-col justify-start items-start flex overflow-hidden">
+      <div class="self-stretch grow shrink basis-0 px-6 py-2 justify-center items-center gap-4 inline-flex overflow-hidden">
+        <div class="grow shrink basis-0 self-stretch flex-col justify-center items-start inline-flex">
+          <div class="self-stretch h-6 text-[#141c26] text-base font-normal font-['Roboto'] leading-normal tracking-wide">230V zásuvka</div>
+        </div>
+        <div class="self-stretch justify-end items-center gap-4 flex">
+          <div class="w-6 h-6 justify-center items-center flex">
+            <div class="p-2 rounded-[100px] justify-center items-center flex">
+              <div class="w-6 h-6 justify-center items-center flex">
+                <div class="w-6 h-6 justify-center items-center inline-flex">
+                  <div class="w-5 h-[21px] text-center text-[#005db8] text-xl font-black font-['Font Awesome 6 Sharp']"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-[360px] h-14 bg-white flex-col justify-start items-start flex overflow-hidden">
+      <div class="self-stretch grow shrink basis-0 px-6 py-2 justify-center items-center gap-4 inline-flex overflow-hidden">
+        <div class="grow shrink basis-0 self-stretch flex-col justify-center items-start inline-flex">
+          <div class="self-stretch h-6 text-[#141c26] text-base font-normal font-['Roboto'] leading-normal tracking-wide">Airbag</div>
+        </div>
+        <div class="self-stretch justify-end items-center gap-4 flex">
+          <div class="w-6 h-6 justify-center items-center flex">
+            <div class="p-2 rounded-[100px] justify-center items-center flex">
+              <div class="w-6 h-6 justify-center items-center flex">
+                <div class="w-6 h-6 justify-center items-center inline-flex">
+                  <div class="w-5 h-[21px] text-center text-[#005db8] text-xl font-black font-['Font Awesome 6 Sharp']"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-[360px] h-14 bg-white flex-col justify-start items-start flex overflow-hidden">
+      <div class="self-stretch grow shrink basis-0 px-6 py-2 justify-center items-center gap-4 inline-flex overflow-hidden">
+        <div class="grow shrink basis-0 self-stretch flex-col justify-center items-start inline-flex">
+          <div class="self-stretch h-6 text-[#141c26] text-base font-normal font-['Roboto'] leading-normal tracking-wide">ABS</div>
+        </div>
+        <div class="self-stretch justify-end items-center gap-4 flex">
+          <div class="w-6 h-6 justify-center items-center flex">
+            <div class="p-2 rounded-[100px] justify-center items-center flex">
+              <div class="w-6 h-6 justify-center items-center flex">
+                <div class="w-6 h-6 justify-center items-center inline-flex">
+                  <div class="w-5 h-[21px] text-center text-[#005db8] text-xl font-black font-['Font Awesome 6 Sharp']"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-[360px] h-14 bg-white flex-col justify-start items-start flex overflow-hidden">
+      <div class="self-stretch grow shrink basis-0 px-6 py-2 justify-center items-center gap-4 inline-flex overflow-hidden">
+        <div class="grow shrink basis-0 self-stretch flex-col justify-center items-start inline-flex">
+          <div class="self-stretch h-6 text-[#141c26] text-base font-normal font-['Roboto'] leading-normal tracking-wide">Alarm</div>
+        </div>
+        <div class="self-stretch justify-end items-center gap-4 flex">
+          <div class="w-6 h-6 justify-center items-center flex">
+            <div class="p-2 rounded-[100px] justify-center items-center flex">
+              <div class="w-6 h-6 justify-center items-center flex">
+                <div class="w-6 h-6 justify-center items-center inline-flex">
+                  <div class="w-5 h-[21px] text-center text-[#005db8] text-xl font-black font-['Font Awesome 6 Sharp']"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-[360px] h-14 bg-white flex-col justify-start items-start flex overflow-hidden">
+      <div class="self-stretch grow shrink basis-0 px-6 py-2 justify-center items-center gap-4 inline-flex overflow-hidden">
+        <div class="grow shrink basis-0 self-stretch flex-col justify-center items-start inline-flex">
+          <div class="self-stretch h-6 text-[#141c26] text-base font-normal font-['Roboto'] leading-normal tracking-wide">Automatické parkování</div>
+        </div>
+        <div class="self-stretch justify-end items-center gap-4 flex">
+          <div class="w-6 h-6 justify-center items-center flex">
+            <div class="p-2 rounded-[100px] justify-center items-center flex">
+              <div class="w-6 h-6 justify-center items-center flex">
+                <div class="w-6 h-6 justify-center items-center inline-flex">
+                  <div class="w-5 h-[21px] text-center text-[#005db8] text-xl font-black font-['Font Awesome 6 Sharp']"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
